ef08249f818feab6a20f0559f18d21d30d60b2cd
bardend <ricardo.ulloa.v@uni.pe>
2023-10-16 09:25:08 -0500

    : )

--- Sketcher.py
+++ Sketcher.py
@@ -1,7 +1,8 @@
 import pygame
 import sys
 from boar import Settings,Boar
-#from Queue import *
+from Queue import *
+sett = set()
 class Sketcher():
     def __init__(self, screen, settings):
         self.screen = screen
@@ -21,21 +22,16 @@ class Sketcher():
                     self.screen.blit(text, (ele.pos[0] + i * self.settings.cell_size + self.settings.cell_size // 2 - text.get_width() // 2, ele.pos[1] + j * self.settings.cell_size + self.settings.cell_size // 2 - text.get_height() // 2))
 
 def run_game():
+    on = 0
     pygame.init()
     settings = Settings()
     screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
     pygame.display.set_caption("8_Puzzles")
     my_boar = Boar(settings.base, (10,300), settings)
-
     sketcher = Sketcher(screen, settings)
-    sketcher.add_boar(my_boar)
 
-    xd = my_boar.gen_boar()
-    print(len(xd))
-
-    
-    for e in xd:
-        sketcher.add_boar(e)
+    q = Queue()
+    q.push(my_boar)
 
     while True:
         for event in pygame.event.get():
@@ -43,10 +39,22 @@ def run_game():
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
-                cnt+=1
+                on = 1
+        
+        while q.isEmpty() == False:
+            v = q.front()
+            if on == 0 : #Apagado
+                break
+            on = 0
+            q.pop()
+            sketcher.add_boar(v)
+            sett.add(v)
+            xd = v.gen_boar()
+            for e in xd:
+                if e not in sett:
+                    q.push(e)
 
         screen.fill(settings.bg_color)
         sketcher.draw()
         pygame.display.flip()
-
 run_game()
